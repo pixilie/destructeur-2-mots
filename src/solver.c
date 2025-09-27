d4098ed96d687536dd9af454bee16ade03070d41
Dischamp <romain.dischamp@epita.fr>
2025-09-27 15:37:36 +0200

    feat: solver branch and prototype

---
+++ src/solver.c
@@ -0,0 +1,22 @@
+
+
+int solver(int argc, char* argv[])
+{
+	/*
+	call type : ./solver filename word
+
+	file type : at least 5 rows and 5 colls of upper letter (square)
+	HORIZONTAL
+	DXRAHCLBGA
+	DIKCILEOKC
+	IGAJHYLYHI
+	HGFGODTIOT
+	GDLROWKBFR
+	PLNRDNERGE
+	JHAIDUAJGV
+	UKGFFOLLEH
+
+	output type : (x1, y1)(x2, y2) -> coo of the first and last letter of the word in the grid
+	*/
+	return 0;
+}
