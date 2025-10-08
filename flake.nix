d351ad149b2649c455c2abf4a839220d8ea2de59
Kristen Couty <kristen.couty@gmail.com>
2025-10-08 13:19:32 +0200

    feat: flake support

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05"; };
+
+  outputs = { self, nixpkgs }:
+    let
+      inherit (nixpkgs.lib) genAttrs;
+
+      forAllSystems =
+        genAttrs [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ];
+      forAllPkgs = function: forAllSystems (system: function pkgs.${system});
+
+      pkgs = forAllSystems (system:
+        (import nixpkgs {
+          inherit system;
+          overlays = [ ];
+        }));
+    in {
+      formatter = forAllPkgs (pkgs: pkgs.nixpkgs-fmt);
+
+      devShells = forAllPkgs (pkgs:
+        with pkgs.lib;
+        let mkClangShell = pkgs.mkShell.override { stdenv = pkgs.clangStdenv; };
+        in {
+          default = mkClangShell rec {
+            nativeBuildInputs = with pkgs; [ ];
+            buildInputs = with pkgs; [ gtk3 ];
+
+            LD_LIBRARY_PATH = makeLibraryPath buildInputs;
+          };
+        });
+    };
+}
