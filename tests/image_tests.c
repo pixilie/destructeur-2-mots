a2c5730d0c5d08e633ba94105d67b9931a6b5069
Liam Chevassus <liam.chevassus@epita.fr>
2025-10-27 18:24:23 +0100

    feat: add image tests

---
+++ tests/image_tests.c
@@ -0,0 +1,162 @@
+#include <stdio.h>
+#include <gdk-pixbuf/gdk-pixbuf.h>
+#include "../include/image.h"
+
+int test_load_image()
+{
+	printf("\n--- Load Image Tests ---\n");
+
+	const char *path = "../assets/level_1_image_1.png";
+	GError *error = NULL;
+	GdkPixbuf *pixbuf = gdk_pixbuf_new_from_file(path, &error);
+
+	if(pixbuf == NULL)
+	{
+		printf("Failed to load image : %s\n", error->message);
+		if(error)
+		{
+			g_error_free(error);
+		}
+		return 0;
+	}
+
+	printf("Successfully loaded image (%dx%d)\n",
+	   gdk_pixbuf_get_width(pixbuf), gdk_pixbuf_get_height(pixbuf));
+
+	g_object_unref(pixbuf);
+	return 1;
+}
+
+int test_grayscale()
+{
+	printf("\n--- Grayscale Tests ---\n");
+
+	const char *path = "../assets/level_1_image_1.png";
+	GdkPixbuf *pixbuf = gdk_pixbuf_new_from_file(path, NULL);
+	if(!pixbuf)
+	{
+		printf("Failed to load image\n");
+		return 0;
+	}
+
+	GdkPixbuf *gray = convert_to_grayscale(pixbuf);
+	if(!gray)
+	{
+		printf("Grayscale failed\n");
+		g_object_unref(pixbuf);
+		return 0;
+	}
+
+	//Check that R = G = B for each pixel (levels of gray)
+	int width = gdk_pixbuf_get_width(gray);
+	int height = gdk_pixbuf_get_height(gray);
+	int n_channels = gdk_pixbuf_get_n_channels(gray);
+	int rowstride = gdk_pixbuf_get_rowstride(gray);
+	guchar *pixels = gdk_pixbuf_get_pixels(gray);
+
+	int ok = 1;
+	for(int y = 0; y < height && ok; y++)
+	{
+		guchar *row = pixels + y * rowstride;
+		for(int x = 0; x < width; x++)
+		{
+			guchar *p = row + x * n_channels;
+			if(!(p[0] == p[1] && p[1] == p[2]))
+			{
+			ok = 0;
+			break;
+			}
+		}
+	}
+
+	if(ok)
+	{
+		printf("[SUCCESS] Grayscale success\n");
+	}
+	else
+	{
+		printf("[FAIL] Grayscale failed\n");
+	}
+	g_object_unref(pixbuf);
+	g_object_unref(gray);
+	return ok;
+}
+
+int test_black_and_white()
+{
+	printf("\n--- Binarize Tests ---\n");
+
+	const char *path = "../assets/level_1_image_1.png";
+	GdkPixbuf *pixbuf = gdk_pixbuf_new_from_file(path, NULL);
+	if(!pixbuf)
+	{
+		printf("[FAIL] Failed to load image\n");
+		return 0;
+	}
+
+	GdkPixbuf *gray = convert_to_grayscale(pixbuf);
+	if(!gray)
+	{
+		printf("[FAIL] Grayscale failed\n");
+		g_object_unref(pixbuf);
+		return 0;
+	}
+
+	GdkPixbuf *black_and_white = convert_to_black_and_white(gray, 128);
+	if(!black_and_white)
+	{
+		printf("[FAIL] Binarize failed");
+		g_object_unref(pixbuf);
+		g_object_unref(gray);
+		return 0;
+	}
+
+	int width = gdk_pixbuf_get_width(black_and_white);
+	int height = gdk_pixbuf_get_height(black_and_white);
+	int n_channels = gdk_pixbuf_get_n_channels(black_and_white);
+	int rowstride = gdk_pixbuf_get_rowstride(black_and_white);
+	guchar *pixels = gdk_pixbuf_get_pixels(black_and_white);
+
+	int ok = 1;
+	for(int y = 0; y < height && ok; y++)
+	{
+		guchar *row = pixels + y * rowstride;
+		for(int x = 0; x < width; x++)
+		{
+			guchar *p = row + x * n_channels;
+			if(!((p[0] == 0 && p[1] == 0 && p[2] == 0) ||
+			(p[0] == 255 && p[1] == 255 && p[2] == 255)))
+			{
+				ok = 0;
+				break;
+			}
+		}
+	}
+
+	if (ok)
+	{
+		printf("[SUCCESS] Successfully converted image to black and white\n");
+	}
+	else
+	{
+		printf("[FAIL] Binarize failed\n");
+	}
+	g_object_unref(pixbuf);
+	g_object_unref(gray);
+	g_object_unref(black_and_white);
+	return ok;
+}
+
+int main()
+{
+	int passed = 1;
+
+	passed &= test_load_image();
+	passed &= test_grayscale();
+	passed &= test_black_and_white();
+
+	printf("\nImage Tests : %s\n", passed ? "ALl tests passed" : "Some tests did not pass");
+
+	return passed ? 0 : 1;
+}
+
