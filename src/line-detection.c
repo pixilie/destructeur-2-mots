d10ceea06f44f34735ba84bbdce32848dab5e82c
Dischamp <romain.dischamp@epita.fr>
2025-10-29 13:19:25 +0100

    chore: just to setup well my branch

--- src/line-detection.c
+++ src/line-detection.c
@@ -2,7 +2,20 @@
 #include <stdlib.h>
 #include "../include/image_rotation.h"
 #include "../include/image_treatment.h"
-#include "../image_slice.h"
+#include "../include/image_slice.h"
+#include "../include/image_helpers.h"
 
 
+
+
+
+
+
+
+
+
+void pipeline(char *filename)
+{
+	GdkPixbuf *pixbuf = *load_image(filename);
+}
 
