6135027afe61f06d154ebb4d50ba70e053b9531c
Ptitjoueur123 <liam.chevassus@epita.fr>
2025-10-29 17:55:29 +0100

    feat: main for image functions
    Co-authored-by: Kristen Couty <kristen.couty@gmail.com>

--- src/image/main.c
+++ src/image/main.c
@@ -1,8 +1,411 @@
+#include "../../include/image_helpers.h"
+#include "../../include/image_rotation.h"
+#include "../../include/image_slice.h"
+#include "../../include/image_treatment.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+// Terminal color codes for pretty output
+#define COLOR_RESET "\033[0m"
+#define COLOR_RED "\033[31m"
+#define COLOR_GREEN "\033[32m"
+#define COLOR_YELLOW "\033[33m"
 
 #ifndef TESTING
-int main(int argc, char **argv) {
-  // TODO: Global executable for images related function
-  return EXIT_SUCCESS;
+
+// Executes image transformations based on command-line arguments
+// Supported functions:
+//   - convert_to_grayscale: Converts an image to grayscale.
+//   - binarize_image: Converts an image to black and white with a threshold.
+//   - rotate_image: Rotates an image by a given angle.
+//   - slice_from: Slices an image horizontally or vertically at given
+//   coordinates.
+//   - slice_in_n: Divides an image into N smaller tiles.
+//
+// Usage:
+//   ./image <function_name> <image_path> <function_params> <optional:output>
+int main(int argc, char **argv)
+{
+    if (argc < 2)
+    {
+        printf(COLOR_RED "[FAIL]" COLOR_RESET " Not enough arguments\n");
+        printf("Usage: /image <function_name> <image_path> <function_params> "
+               "<optional:output>\n");
+        printf("Please specify a function name\n");
+        printf("Available functions:\n");
+        printf(
+            " - ./image convert_to_grayscale <image_path> <optional:output>\n");
+        printf(" - ./image binarize_image <image_path> <threshold> "
+               "<optional:output>\n");
+        printf(" - ./image rotate_image <image_path> <angle_degrees> "
+               "<optional:output>\n");
+        printf(" - ./image slice_from <image_path> <x> <y> <direction> "
+               "<optional:output1> <optional:output2>\n");
+        printf(" - ./image slice_in_n <image_path> <n_slice> "
+               "<optional:output_prefix>\n");
+        return EXIT_FAILURE;
+    }
+
+    char *function_name = argv[1];
+
+    // Check function name
+    if (strcmp(function_name, "convert_to_grayscale") &&
+        strcmp(function_name, "binarize_image") &&
+        strcmp(function_name, "rotate_image") &&
+        strcmp(function_name, "slice_from") &&
+        strcmp(function_name, "slice_in_n"))
+    {
+        printf(COLOR_RED "[FAIL]" COLOR_RESET
+                         " Incorrect function name, got: " COLOR_RED
+                         "%s\n" COLOR_RESET,
+               function_name);
+        printf("Usage: ./image <function_name> <image_path> <function_params> "
+               "<optional:output>\n");
+        printf("Available functions:\n");
+        printf(
+            " - ./image convert_to_grayscale <image_path> <optional:output>\n");
+        printf(" - ./image binarize_image <image_path> <threshold> "
+               "<optional:output>\n");
+        printf(" - ./image rotate_image <image_path> <angle_degrees> "
+               "<optional:output>\n");
+        printf(" - ./image slice_from <image_path> <x> <y> <direction> "
+               "<optional:output1> <optional:output2>\n");
+        printf(" - ./image slice_in_n <image_path> <n_slice> "
+               "<optional:output_prefix>\n");
+        return EXIT_FAILURE;
+    }
+    else
+    {
+        // convert_to_grayscale
+        if (strcmp(function_name, "convert_to_grayscale") == 0)
+        {
+            if (argc < 3)
+            {
+                printf(COLOR_RED
+                       "[FAIL]" COLOR_RESET
+                       " Not enough arguments for convert_to_grayscale\n");
+                printf("Usage: ./image convert_to_grayscale <image_path> "
+                       "<optional:output>\n");
+                return EXIT_FAILURE;
+            }
+
+            // Load image
+            GdkPixbuf *pixbuf = load_image(argv[2]);
+            if (!pixbuf)
+            {
+                printf(COLOR_RED "[FAIL]" COLOR_RESET
+                                 " Image could not be loaded\n");
+                return EXIT_FAILURE;
+            }
+
+            convert_to_grayscale(pixbuf);
+            if (argc > 3)
+            {
+                save_pixbuf_as_png(pixbuf, argv[3]);
+                printf(COLOR_GREEN
+                       "[SUCCESS]" COLOR_RESET
+                       " Image %s converted to grayscale and saved as %s\n",
+                       argv[2], argv[3]);
+            }
+            else
+            {
+                save_pixbuf_as_png(pixbuf, "grayscaled.png");
+                printf(COLOR_GREEN "[SUCCESS]" COLOR_RESET
+                                   " Image %s converted to grayscale and saved "
+                                   "as grayscaled.png\n",
+                       argv[2]);
+            }
+            g_object_unref(pixbuf);
+            return EXIT_SUCCESS;
+        }
+
+        // binarize_image
+        if (strcmp(function_name, "binarize_image") == 0)
+        {
+            if (argc < 4)
+            {
+                printf(COLOR_RED "[FAIL]" COLOR_RESET
+                                 " Not enough arguments for binarize_image\n");
+                printf("Usage: ./image binarize_image <image_path> <threshold> "
+                       "<optional:output>\n");
+                return EXIT_FAILURE;
+            }
+
+            // Load image
+            GdkPixbuf *pixbuf = load_image(argv[2]);
+            if (!pixbuf)
+            {
+                printf(COLOR_RED "[FAIL]" COLOR_RESET
+                                 " Image could not be loaded\n");
+                return EXIT_FAILURE;
+            }
+
+            int threshold = strtol(argv[3], NULL, 10);
+            binarize_image(pixbuf, threshold);
+            if (argc > 4)
+            {
+                save_pixbuf_as_png(pixbuf, argv[4]);
+                printf(COLOR_GREEN "[SUCCESS]" COLOR_RESET
+                                   " Image %s converted to black and white "
+                                   "with threshold %i and saved as %s\n",
+                       argv[2], threshold, argv[4]);
+            }
+            else
+            {
+                save_pixbuf_as_png(pixbuf, "binarized.png");
+                printf(COLOR_GREEN
+                       "[SUCCESS]" COLOR_RESET
+                       " Image %s converted to black and white with threshold "
+                       "%i and saved as binarized.png\n",
+                       argv[2], threshold);
+            }
+            g_object_unref(pixbuf);
+            return EXIT_SUCCESS;
+        }
+
+        // rotate_image
+        if (strcmp(function_name, "rotate_image") == 0)
+        {
+            if (argc < 4)
+            {
+                printf(COLOR_RED "[FAIL]" COLOR_RESET
+                                 " Not enough arguments for rotate_image\n");
+                printf("Usage: ./image rotate_image <image_path> "
+                       "<angle_degrees> <optional:output>\n");
+                return EXIT_FAILURE;
+            }
+
+            // Load image
+            GdkPixbuf *pixbuf = load_image(argv[2]);
+            if (!pixbuf)
+            {
+                printf(COLOR_RED "[FAIL]" COLOR_RESET
+                                 " Image could not be loaded\n");
+                return EXIT_FAILURE;
+            }
+
+            double angle_degrees = strtod(argv[3], NULL);
+            GdkPixbuf *rotated = rotate_image(pixbuf, angle_degrees);
+            if (!rotated)
+            {
+                printf(COLOR_RED "[FAIL]" COLOR_RESET
+                                 " Rotate function failed\n");
+                g_object_unref(pixbuf);
+                return EXIT_FAILURE;
+            }
+            if (argc > 4)
+            {
+                save_pixbuf_as_png(rotated, argv[4]);
+                printf(COLOR_GREEN
+                       "[SUCCESS]" COLOR_RESET
+                       " Image %s rotated by %.2f degrees and saved as %s\n",
+                       argv[2], angle_degrees, argv[4]);
+            }
+            else
+            {
+                save_pixbuf_as_png(rotated, "rotated.png");
+                printf(COLOR_GREEN "[SUCCESS]" COLOR_RESET
+                                   " Image %s rotated by %.2f degrees and "
+                                   "saved as rotated.png\n",
+                       argv[2], angle_degrees);
+            }
+            g_object_unref(pixbuf);
+            g_object_unref(rotated);
+            return EXIT_SUCCESS;
+        }
+
+        // slice_from
+        if (strcmp(function_name, "slice_from") == 0)
+        {
+            if (argc < 6)
+            {
+                printf(COLOR_RED "[FAIL]" COLOR_RESET
+                                 " Not enough arguments for slice_from\n");
+                printf("Usage: ./image slice_from <image_path> <x> <y> "
+                       "<direction> <optional:output1> <optional:output2>\n");
+                return EXIT_FAILURE;
+            }
+
+            // Load image
+            GdkPixbuf *pixbuf = load_image(argv[2]);
+            if (!pixbuf)
+            {
+                printf(COLOR_RED "[FAIL]" COLOR_RESET
+                                 " Image could not be loaded\n");
+                return EXIT_FAILURE;
+            }
+
+            int x = (int)strtol(argv[3], NULL, 10);
+            int y = (int)strtol(argv[4], NULL, 10);
+            int direction = (int)strtol(argv[5], NULL, 10);
+            GdkPixbuf **pixbufs = slice_from(pixbuf, x, y, direction);
+            if (!pixbufs)
+            {
+                printf(COLOR_RED "[FAIL]" COLOR_RESET " Slice_from failed\n");
+                g_object_unref(pixbuf);
+                return EXIT_FAILURE;
+            }
+
+            GdkPixbuf *pixbuf1 = pixbufs[0];
+            GdkPixbuf *pixbuf2 = pixbufs[1];
+            if (direction == 1) // horiontal cut
+            {
+                printf(COLOR_GREEN "[SUCCESS]" COLOR_RESET
+                                   " Image %s has been sliced horizontally at "
+                                   "y = %i into 2 smaller images\n",
+                       argv[2], y);
+            }
+            else // vertical cut
+            {
+                printf(COLOR_GREEN "[SUCCESS]" COLOR_RESET
+                                   " Image %s has been sliced vertically at x "
+                                   "= %i into 2 smaller images\n",
+                       argv[2], x);
+            }
+
+            if (argc > 6)
+            {
+                save_pixbuf_as_png(pixbuf1, argv[6]);
+                if (direction == 1)
+                {
+                    printf(COLOR_GREEN "[SUCCESS]" COLOR_RESET
+                                       " Top image has been saved as %s\n",
+                           argv[6]);
+                }
+                else
+                {
+                    printf(COLOR_GREEN "[SUCCESS]" COLOR_RESET
+                                       " Left image has been saved as %s\n",
+                           argv[6]);
+                }
+                if (argc > 7)
+                {
+                    save_pixbuf_as_png(pixbuf2, argv[7]);
+                    if (direction == 1)
+                    {
+                        printf(COLOR_GREEN
+                               "[SUCCESS]" COLOR_RESET
+                               " Bottom image has been saved as %s\n",
+                               argv[7]);
+                    }
+                    else
+                    {
+                        printf(COLOR_GREEN
+                               "[SUCCESS]" COLOR_RESET
+                               " Right image has been saved as %s\n",
+                               argv[7]);
+                    }
+                }
+                else
+                {
+                    if (direction == 1)
+                    {
+                        save_pixbuf_as_png(pixbuf2, "sliced_image_bottom.png");
+                        printf(COLOR_GREEN "[SUCCESS]" COLOR_RESET
+                                           " Bottom image has been saved as "
+                                           "sliced_image_bottom.png\n");
+                    }
+                    else
+                    {
+                        save_pixbuf_as_png(pixbuf2, "sliced_image_right.png");
+                        printf(COLOR_GREEN "[SUCCESS]" COLOR_RESET
+                                           " Right image has been saved as "
+                                           "sliced_image_right.png\n");
+                    }
+                }
+            }
+            else
+            {
+                if (direction == 1)
+                {
+                    save_pixbuf_as_png(pixbuf1, "sliced_image_top.png");
+                    printf(
+                        COLOR_GREEN
+                        "[SUCCESS]" COLOR_RESET
+                        " Top image has been saved as sliced_image_top.png\n");
+                    save_pixbuf_as_png(pixbuf2, "sliced_image_bottom.png");
+                    printf(COLOR_GREEN "[SUCCESS]" COLOR_RESET
+                                       " Bottom image has been saved as "
+                                       "sliced_image_bottom.png\n");
+                }
+                else
+                {
+                    save_pixbuf_as_png(pixbuf1, "sliced_image_left.png");
+                    printf(COLOR_GREEN "[SUCCESS]" COLOR_RESET
+                                       " Left image has been saved as "
+                                       "sliced_image_left.png\n");
+                    save_pixbuf_as_png(pixbuf2, "sliced_image_right.png");
+                    printf(COLOR_GREEN "[SUCCESS]" COLOR_RESET
+                                       " Right image has been saved as "
+                                       "sliced_image_right.png\n");
+                }
+            }
+            g_object_unref(pixbuf);
+            g_object_unref(pixbufs[0]);
+            g_object_unref(pixbufs[1]);
+            free(pixbufs);
+            return EXIT_SUCCESS;
+        }
+
+        // slice_in_n
+        if (strcmp(function_name, "slice_in_n") == 0)
+        {
+            if (argc < 4)
+            {
+                printf(COLOR_RED "[FAIL]" COLOR_RESET
+                                 " Not enough arguments for slice_in_n\n");
+                printf("Usage: ./image slice_in_n <image_path> <n_slice> "
+                       "<optional:output_prefix>\n");
+                return EXIT_FAILURE;
+            }
+
+            // Load image
+            GdkPixbuf *pixbuf = load_image(argv[2]);
+            if (!pixbuf)
+            {
+                printf(COLOR_RED "[FAIL]" COLOR_RESET
+                                 " Image could not be loaded\n");
+                return EXIT_FAILURE;
+            }
+
+            int n_slice = (int)strtol(argv[3], NULL, 10);
+            GdkPixbuf **tiles = slice_in_n(pixbuf, n_slice);
+            if (!tiles)
+            {
+                printf(COLOR_RED "[FAIL]" COLOR_RESET " Slice_in_n failed\n");
+                g_object_unref(pixbuf);
+                return EXIT_FAILURE;
+            }
+            printf(COLOR_GREEN "[SUCCESS]" COLOR_RESET
+                               " Image %s has been sliced into %i tiles\n",
+                   argv[2], n_slice);
+
+            const char *prefix;
+            if (argc > 4)
+            {
+                prefix = argv[4];
+            }
+            else
+            {
+                prefix = "sliced_in_n";
+            }
+
+            char filename[256];
+            for (int i = 0; i < n_slice; i++)
+            {
+                snprintf(filename, sizeof(filename), "%s_%i.png", prefix, i);
+                save_pixbuf_as_png(tiles[i], filename);
+                printf(COLOR_GREEN "[SUCCESS]" COLOR_RESET
+                                   " Tile #%i has been saved as %s\n",
+                       i, filename);
+                g_object_unref(tiles[i]);
+            }
+
+            free(tiles);
+            g_object_unref(pixbuf);
+            return EXIT_SUCCESS;
+        }
+    }
 }
 #endif
