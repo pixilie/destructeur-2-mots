60f21b38d6d3d8391c6e959f8fa31d4535818e4f
Erouin <romain.dischamp@gmail.com>
2025-10-31 14:18:44 +0100

    fix: include fix

--- tests/line_detection_tests.c
+++ tests/line_detection_tests.c
@@ -1,6 +1,11 @@
 #include "../include/line_detection.h"
 
-int main(int argc, char *argv[])
+int test_pipeline(char *filename, char *output_gw_file, char *output_letter_file)
 {
-    pipeline("../assets/level_1_image_1.png", "results/gw", "results/letters");
+	pipeline(filename, output_gw_file, output_letter_file);
+}
+
+int main()
+{
+	int test = test_pipeline("../assets/level_1_image_1","result/grid_and_word","result/letter")   
 }
