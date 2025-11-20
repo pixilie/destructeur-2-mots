98b6845af527b670195a87f4f6dcf97963f635a2
Liam Chevassus <liam.chevassus@epita.fr>
2025-11-20 22:26:56 +0100

    chore: update include/image_rotation.h with new function downscale_pixbuf and change parameters names to pixbuf instead of src

--- include/image_rotation.h
+++ include/image_rotation.h
@@ -2,7 +2,8 @@
 
 #include <gdk-pixbuf/gdk-pixbuf.h>
 
-GdkPixbuf *rotate_image(GdkPixbuf *src, double angle_degrees);
-double compute_projection_variance(GdkPixbuf *pix);
-double detect_best_angle(GdkPixbuf *src);
+GdkPixbuf *rotate_image(GdkPixbuf *pixbuf, double angle_degrees);
+GdkPixbuf *downscale_pixbuf(GdkPixbuf *pixbuf, int target_width);
+double compute_projection_variance(GdkPixbuf *pixbuf);
+double detect_best_angle(GdkPixbuf *pixbuf);
 
