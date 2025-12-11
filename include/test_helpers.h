1978334f7c649fc911e7b601684b831162d83727
Kristen Couty <kristen.couty@gmail.com>
2025-12-11 17:58:09 +0100

    chore: cleaning code for submit

--- include/test_helpers.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-void print_fail();
-void print_success();
-void print_all_tests_passed(const char *test_name);
-void print_some_tests_failed(const char *test_name);
-void print_test_category(const char *test_category);
-void print_test_subcategory(const char *test_subcategory);
-void print_test_subsubcategory(const char *test_subsubcategory);
